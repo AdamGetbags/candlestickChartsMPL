efca326ac6be59ea733a23506991c3d7cb81ce96
Adam Getbags <91522359+AdamGetbags@users.noreply.github.com>
2022-08-28 14:01:36 -0700

    Update plotCandlesticks.py

--- plotCandlesticks.py
+++ plotCandlesticks.py
@@ -10,18 +10,18 @@ from tda import auth, client
 import json
 import pandas as pd
 from datetime import datetime
-import TDAsecrets
+import secretsTDA
 import mplfinance as mpf #pip install mplfinance
 
 #authentication flow
 try:
-    c = auth.client_from_token_file(TDAsecrets.token_path, TDAsecrets.api_key)
+    c = auth.client_from_token_file(secretsTDA.token_path, secretsTDA.api_key)
 except FileNotFoundError:
     from selenium import webdriver
     with webdriver.Chrome() as driver:
         c = auth.client_from_login_flow(
-            driver, TDAsecrets.api_key, TDAsecrets.redirect_uri,
-            TDAsecrets.token_path)
+            driver, secretsTDA.api_key, secretsTDA.redirect_uri,
+            secretsTDA.token_path)
 
 #request data
 r = c.get_price_history('XOM',
